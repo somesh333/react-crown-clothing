350b830961c2a96bfa53c61f8b921234036f0564
unknown <someshjha.333@gmail.com>
2023-01-08 22:12:51 +0530

    improve the contact file and detail file

--- src/components/Detail/detail.js
+++ src/components/Detail/detail.js
@@ -19,11 +19,7 @@ const detail = () => {
         <label for="lname">Last Name</label>
         {/* <input type="text" id="lname" name="lastname" placeholder="Your last name.."> */}
         <label for="country">Country</label>
-        <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
-        </select>
+       
         <label for="subject">Subject</label>
         <textarea id="subject" name="subject" placeholder="Write something.." style="height:170px"></textarea>
         {/* <input type="submit" value="Submit"> */}
