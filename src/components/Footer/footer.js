2d73f01a556f55825dcc82a72651a73a448a8e6a
unknown <someshjha.333@gmail.com>
2023-01-12 22:42:40 +0530

    create footer in clothes web app

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+        <footer>
+            <div class="column">
+                 
+ 
+<p>Company</p>
+ 
+ 
+                <ul style="list-style-type:disc">
+                    <li>About Us</li>
+                    <li>Careers</li>
+                    <li>Privacy Policy</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+               
+            <div class="column">
+                 
+ 
+<p>Learn</p>
+ 
+ 
+                <ul>
+                    <li>Algorithms</li>
+                    <li>Data Structures</li>
+                    <li>Languages</li>
+                    <li>CS Subjects</li>
+                    <li>Video Tutorials</li>
+                </ul>
+            </div>
+                
+            <div class="column">
+                 
+ 
+<p>Practice</p>
+ 
+ 
+                <ul>
+                    <li>Company-wise</li>
+                    <li>Topic-wise</li>
+                    <li>Contests</li>
+                    <li>Subjective Questions</li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default footer
