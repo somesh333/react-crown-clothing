0fbd9ee603b788cda0becfcf1c2c795ae428c83f
unknown <someshjha.333@gmail.com>
2023-01-27 23:50:07 +0530

    create product page

---
+++ src/components/New_Component/Detail/product.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <div><div class="card">
+    {/* <img src="jeans3.jpg" alt="Denim Jeans" style="width:100%"> */}
+    <h1>Tailored Jeans</h1>
+    <p class="price">$19.99</p>
+    <p>Some text about the jeans..</p>
+    <p><button>Add to Cart</button></p>
+  </div></div>
+  )
+}
+
+export default product
